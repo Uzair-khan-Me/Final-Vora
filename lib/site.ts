7a842343c0f76ff9b1296465fa6e6ba7cfe014ef
ko <uk726433@gmail.com>
2026-08-19 12:17:01 +0500

    Update developer information in site.ts
    Removed GITHUB_REPO constant and added DEVELOPER_PORTFOLIO constant.

--- lib/site.ts
+++ lib/site.ts
@@ -36,10 +36,9 @@ export const APK_URL =
 
 export const APK_SIZE = '160 MB';
 
-export const GITHUB_REPO = 'https://github.com/Uzair-khan-Me/Final-Vora';
-
 export const DEVELOPER_NAME = 'Uzair Ali';
 export const DEVELOPER_ROLE = 'Developer of Final Vora';
+export const DEVELOPER_PORTFOLIO = 'https://uzairali-18.github.io/Portfolio/';
 export const DEVELOPER_EMAIL = 'uzairali10005@gmail.com';
 export const DEVELOPER_IMAGE = '/images/uzair-ali.jpg';
 
